e2ce4b36675323caf8fdd8b16ad30ac102d10a02
artem <artemasarkisan598@gmail.com>
2025-11-14 15:02:24 +0200

    py-Check password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,14 @@
-# write your code here
+from app.main import check_password
+import pytest
+
+
+@pytest.mark.parametrize(
+    "password, exception",
+    [
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False)
+    ]
+)
+def test_check_password(password: str, exception: bool) -> None:
+    assert check_password(password) == exception
